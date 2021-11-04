456e1dd19a7b7c9e43eb47a5d9cbf55bf896cecb
C:/Program Files/Git/lzoovalm <lazo.ortiz.valeria.montserrat@gmail.com>
2021-11-04 17:28:19 -0600

    modificacion al holamundo3.java

--- HolaMundo3.java
+++ HolaMundo3.java
@@ -4,7 +4,8 @@ public class HolaMundo3
 	{
 		System.out.println("Hola mundo3");
 		System.out.println("Hola Arturo Osuna");
-        	System.out.println("Hola Lalo");
+		System.out.println("Hola Lalo");
+		System.out.println("Hola Valeria");
 	}
 }
 
