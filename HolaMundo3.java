7c63d578c42facae4a06caf2e393f294af51b426
LustrilloMendozaEduardoArturo <Lustrillomendozaeduardoarturo@gmail.com>
2021-11-04 16:55:07 -0600

    2DO Intento

--- HolaMundo3.java
+++ HolaMundo3.java
@@ -1,9 +1,10 @@
 public class HolaMundo3
-
 {
         public void main( String a[])
-        {
-		System.out.prinntln("Hola Arturo Osuna");
-        }
+	{
+		System.out.println("Hola mundo3");
+		System.out.println("Hola Arturo Osuna");
+        	System.out.println("Hola Lalo");
+	}
 }
 
